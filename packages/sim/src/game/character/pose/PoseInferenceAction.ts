24b83f9f6cb77f500f246bb9875b19b625089b7a
brudahdev <alpha.d3v@gmail.com>
2026-06-20 14:08:01 -0400

    pose inference action desc update

--- packages/sim/src/game/character/pose/PoseInferenceAction.ts
+++ packages/sim/src/game/character/pose/PoseInferenceAction.ts
@@ -4,7 +4,7 @@ import { Entity } from "../../../core/ec/Entity"
 import { EntityRegistry } from "../../entity/EntityRegistry"
 import { EventSystem } from "../../EventSystem"
 import { buildAvatarPrompt } from "../characterViews"
-import { CharacterIdentityKey } from "../identity/CharacterIdentity"
+import { CharacterIdentity, CharacterIdentityKey } from "../identity/CharacterIdentity"
 import { resolveTargetCharacter } from "../identity/characterLookup"
 import { CharacterPoseKey } from "./CharacterPose"
 
@@ -21,27 +21,28 @@ type PoseInferenceArgs = typeof PoseInferenceArgs
 export class PoseInferenceAction extends CharacterInferenceAction<PoseInferenceArgs> {
 
     readonly args = PoseInferenceArgs
-    private readonly characterName: string;
 
+    private readonly charId: CharacterIdentity
     constructor(
         private entity: Entity,
         private eventSystem: EventSystem,
         manager: InferenceActionManager,
         private readonly registry: EntityRegistry,
     ) {
-        const characterName = entity.require(CharacterIdentityKey).name
+        const charId = entity.require(CharacterIdentityKey);
+
         super({
-            name: `${characterName}_change_pose`, characterId: entity.id,
+            name: `${charId.name}_change_pose`, characterId: entity.id,
             layer: 'act',
             before: false
         }, manager)
-        this.characterName = characterName;
+        this.charId = charId;
         this.init();
     }
 
     protected computeContent(): ActionContent<PoseInferenceArgs> {
         return {
-            description: `When ${this.characterName} alteres the state of a piece of Pose. Do NOT call this if its only a request, command, or hypothetical`,
+            description: `When ${this.charId.name} changes ${this.charId.config.pronouns.himHer} or another's pose. Do NOT call this if its only a request, command, or hypothetical`,
             argDescriptions: {
                 subjectName: 'The name of the person whose pose changed.',
                 newPoseName: 'The name of the pose to change to type:[stand|sit|squat|kneel|lay_on_stomach|lay_on_side|lay_on_back|bent_over] (non exaustive, you may pick others)',
